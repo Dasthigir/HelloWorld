52db68d0971c4530890cc56e79c8bda8e9df1fd2
Dasthigir Baig <56359465+Dasthigir@users.noreply.github.com>
2024-05-30 21:49:50 +0530

    Update EmailPlugin.groovy

--- EmailPlugin.groovy
+++ EmailPlugin.groovy
@@ -1,34 +1,52 @@
-import com.NTT.HPHC.CTS.Email.*;
-import com.urbancode.air.AirPluginTool
-final def airTool = new AirPluginTool(this.args[0], this.args[1])
-final def props = airTool.getStepProperties()
-final def varFromEmailList = props['fromMail']
-final def varToEmailList = props['toList']
-final def varCCEmailList = props['ccList']
-final def varEmailSubjectr = props['subject']
-final def varEmailMessage = props['emailMessage']
-final def varEmailQueueServer = props['emailQueueServer']
-final def varEmailQueueName = props['emailQueueName']
-final def varEmailQueueUser = props['emailQueueUser']
-final def varEmailQueuePwd = props['emailQueuePwd']
+import com.NTT.HPHC.CTS.Email.MessageIntoQueue;
+import com.urbancode.air.AirPluginTool;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-final def scriptDir = System.getenv('PLUGIN_HOME')
+public class EmailSender {
+    public static void main(String[] args) {
+        try {
+            // Initialize AirPluginTool
+            AirPluginTool airTool = new AirPluginTool(args[0], args[1]);
+            var props = airTool.getStepProperties();
 
+            // Retrieve properties
+            String varFromEmailList = props.get("fromMail");
+            String varToEmailList = props.get("toList");
+            String varCCEmailList = props.get("ccList");
+            String varEmailSubjectr = props.get("subject");
+            String varEmailMessage = props.get("emailMessage");
+            String varEmailQueueServer = props.get("emailQueueServer");
+            String varEmailQueueName = props.get("emailQueueName");
+            String varEmailQueueUser = props.get("emailQueueUser");
+            String varEmailQueuePwd = props.get("emailQueuePwd");
 
-String emailFileContents = new File( scriptDir + '\\EmailTemplate.xml').text
+            // Get the plugin home directory
+            String scriptDir = System.getenv("PLUGIN_HOME");
 
-emailFileContents = emailFileContents.replace("FromEmailList",varFromEmailList)
-emailFileContents = emailFileContents.replace("ToReplyTo",varFromEmailList)
-emailFileContents = emailFileContents.replace("ToEmailList",varToEmailList)
-emailFileContents = emailFileContents.replace("CCEmailList",varCCEmailList)
-emailFileContents = emailFileContents.replace("EmailSubjectToSend",varEmailSubjectr)
-emailFileContents = emailFileContents.replace("EmailMessage",varEmailMessage)
+            // Read the email template file
+            String emailFileContents = new String(Files.readAllBytes(Paths.get(scriptDir, "EmailTemplate.xml")));
 
-def EmailMessageFile = scriptDir + "/EmailMessage.xml"
+            // Replace placeholders with actual values
+            emailFileContents = emailFileContents.replace("FromEmailList", varFromEmailList)
+                                                 .replace("ToReplyTo", varFromEmailList)
+                                                 .replace("ToEmailList", varToEmailList)
+                                                 .replace("CCEmailList", varCCEmailList)
+                                                 .replace("EmailSubjectToSend", varEmailSubjectr)
+                                                 .replace("EmailMessage", varEmailMessage);
 
-File file = new File(EmailMessageFile).write(emailFileContents)
+            // Write the updated content to a new file
+            String emailMessageFilePath = Paths.get(scriptDir, "EmailMessage.xml").toString();
+            Files.write(Paths.get(emailMessageFilePath), emailFileContents.getBytes());
 
-MessageIntoQueue MIQ = new MessageIntoQueue(); 
-MIQ.sendTopicMessage(varEmailQueueServer,varEmailQueueName,varEmailQueueUser, varEmailQueuePwd,EmailMessageFile);
-	
+            // Send the email message
+            MessageIntoQueue MIQ = new MessageIntoQueue();
+            MIQ.sendTopicMessage(varEmailQueueServer, varEmailQueueName, varEmailQueueUser, varEmailQueuePwd, emailMessageFilePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
